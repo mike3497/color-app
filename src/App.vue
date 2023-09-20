e263f87e583a64096b3c638ca7d289f09853a691
Michael Carillon <mcarillon97@gmail.com>
2023-09-20 19:58:25 -0400

    update header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="flex flex-col h-screen">
-    <header class="p-4">
-      <div>
-        <h1 class="text-4xl font-bold mb-2">Colorify</h1>
-        <p>Press spacebar to generate color palettes!</p>
+    <header>
+      <div class="p-4 border-b border-b-zinc-200">
+        <div class="flex items-center gap-2 text-2xl font-bold">
+          <span class="material-symbols-outlined">palette</span>
+          <h1>Colorify</h1>
+        </div>
       </div>
+      <p class="p-4">Press spacebar to generate color palettes!</p>
     </header>
     <div class="flex-1">
       <div class="grid grid-cols-5 h-full">
