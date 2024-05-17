cef8bc4540e3cbe488b9577fed9065c7f1878a3d
Michael Carillon <mcarillon97@gmail.com>
2024-05-17 13:11:17 -0500

    feat: add updated year to footer

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,10 +1,15 @@
 <template>
   <footer class="bg-zinc-800 text-zinc-400 p-4">
     <div>
-      <p class="text-sm">
-        &copy; Copyright 2023
+      <p class="text-center text-sm">
+        <span>&copy; Copyright {{ year }}</span>
+        <span>&nbsp;</span>
         <a class="underline" href="https://michaelcarillon.com/">Michael Carillon</a>
       </p>
     </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
